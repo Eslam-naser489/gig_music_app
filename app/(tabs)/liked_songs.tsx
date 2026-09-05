156505436b889750fb02904c554e1b2c3fef2b14
Sherif <sherif@gmail.com>
2026-09-05 19:10:15 +0300

    add SongListItem component with props

--- app/(tabs)/liked_songs.tsx
+++ app/(tabs)/liked_songs.tsx
@@ -3,7 +3,16 @@ import { SafeAreaView } from "react-native";
 export default function LikedSongs() {
   return (
     <SafeAreaView>
-      <SongListItem />
+      <SongListItem
+        song={{
+          id: 1,
+          title: "Africa",
+          artist: "Toto",
+          cover_url:
+            "https://cdn-images.dzcdn.net/images/cover/153332e88a14255a8c3d5959a5a21882/500x500-000000-80-0-0.jpg",
+          duration: 295,
+        }}
+      />
     </SafeAreaView>
   );
 }
--- components/play_lists/song_list_item.tsx
+++ components/play_lists/song_list_item.tsx
@@ -1,13 +1,27 @@
 import { Ionicons } from "@expo/vector-icons";
-import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
-export default function SongListItem() {
+import { Image, Text, View } from "react-native";
+type SongListItemProps = {
+  song: {
+    id: number;
+    title: string;
+    artist: string;
+    cover_url: string;
+    duration: number;
+  };
+};
+export default function SongListItem({song}: SongListItemProps) {
   return (
-    <View>
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
       <Image
-        source={{ uri: "حط_هنا_رابط_cover_url" }}
+        source={{
+          uri: song.cover_url
+        }}
         style={{ width: 50, height: 50 }}
       />
-      <Text>Africa</Text>
+      <View>
+        <Text>{song.title}</Text>
+        <Text style={{ color: "gray", fontSize: 13 }}>{song.artist}</Text>
+      </View>
       <Ionicons name="heart" size={24} color="#FF5A3C" />
     </View>
   );
